cad373db0ac7ac669d85e0ccf6c2b7be36ea14c2
Huy <lsqhuy@gmail.com>
2022-03-11 21:48:10 +0700

    feat: mongo

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 // import compression from 'compression' // compresses requests
-// import session from 'express-session'
+import session from "express-session";
 import bodyParser from "body-parser";
 import lusca from "lusca";
-// import MongoStore from 'connect-mongo'
+import MongoStore from "connect-mongo";
 // import flash from 'express-flash'
 import path from "path";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 // import passport from "passport";
-// import bluebird from "bluebird";
-// import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+import bluebird from "bluebird";
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
+import * as breadController from "./controllers/bread";
 import * as userController from "./controllers/user";
 import * as apiController from "./controllers/api";
 import * as contactController from "./controllers/contact";
@@ -24,15 +25,18 @@ import * as contactController from "./controllers/contact";
 const app = express();
 
 // Connect to MongoDB
-// const mongoUrl = MONGODB_URI;
-// mongoose.Promise = bluebird;
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
 
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-//     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-// ).catch(err => {
-//     console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-//     // process.exit();
-// });
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+  () => { 
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
+    console.log("Connect MongoDB success");
+  },
+).catch(err => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
@@ -41,17 +45,17 @@ app.set("view engine", "pug");
 // app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(session({
-//     resave: true,
-//     saveUninitialized: true,
-//     secret: SESSION_SECRET,
-//     store: new MongoStore({
-//         mongoUrl,
-//         mongoOptions: {
-//             autoReconnect: true
-//         }
-//     })
-// }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+        mongoUrl,
+        mongoOptions: {
+            autoReconnect: true
+        }
+    })
+}));
 // app.use(passport.initialize());
 // app.use(passport.session());
 // app.use(flash())
@@ -94,6 +98,7 @@ app.get("/signup", userController.getSignup);
 app.post("/signup", userController.postSignup);
 app.get("/contact", contactController.getContact);
 app.post("/contact", contactController.postContact);
+app.get("/breads", breadController.index);
 // app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
 // app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
 // app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
@@ -104,6 +109,7 @@ app.post("/contact", contactController.postContact);
  * API examples routes.
  */
 app.get("/api", apiController.getApi);
+app.post("/api/create/bread", breadController.postBread);
 // app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
 
 /**
---
+++ src/models/Bread.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+type BreadType = "long" | "circle";
+
+export type BreadDocument = mongoose.Document & {
+  name: string;
+  type: BreadType,
+};
+
+const breadSchema = new mongoose.Schema<BreadDocument>(
+  {
+    name: { type: String, unique: true },
+    type: { type: String },
+  },
+  { timestamps: true },
+);
+export const Bread = mongoose.model<BreadDocument>("Bread", breadSchema);
